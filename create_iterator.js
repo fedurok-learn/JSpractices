86d7fb536429a2f49d8c9d71c2b7e40faebd7549
B1Z0N <32279961+B1Z0N@users.noreply.github.com>
2019-02-24 16:30:16 +0200

    Added iterators for custom objects functionality

---
+++ create_iterator.js
@@ -0,0 +1,42 @@
+"use strict"
+
+let createIterator = function (obj) {
+    Object.defineProperty(
+        obj, 
+        Symbol.iterator,
+        {
+            writable: false,
+            enumerable: false,
+            configurable: true,
+            value: function () {
+                let o = this;
+                let idx = 0;
+                let props = Object.keys( o );
+
+                return {
+                    next: function() {
+                        return {
+                            value: o[ props[ idx++ ] ],
+                            done: idx > props.length
+                        }
+                    }
+                }
+            }
+        }
+    );
+}
+
+
+let myobj = {
+    a: 2,
+    b: 4,
+    c: 6
+};
+
+createIterator(myobj);
+
+let it = myobj[Symbol.iterator]();
+
+for(let i of myobj) {
+    console.log( i );
+}
