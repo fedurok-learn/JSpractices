4256d21bd0e8851761aaeecfdbbd2a21b590a8bc
B1Z0N <32279961+B1Z0N@users.noreply.github.com>
2019-03-09 21:07:23 +0200

    Scrapper is functioning. But some photos still not downloading because of Max Listeners Exceeded. So i will try to optimize it in future. Also code need to be refactored. And photo names should be taken care of.

--- scrapping/instaPhotoScrapper/instaScrapper.js
+++ scrapping/instaPhotoScrapper/instaScrapper.js
@@ -1,33 +1,61 @@
 "use strict";
 
 const $ = require("cheerio");
+const fs = require('fs');
+const rp = require('request-promise');
+
 
 const getHTML = require("../getRightHTML");
 const asyncf = require("../asyncMap");
 
 const url = "https://www.instagram.com/_headless_nick_/"
+const PATH =  __dirname + "/Photos/"
 const addUrl = 'https://www.instagram.com'
 
 
+String.prototype.hashCode = function() {
+    var hash = 0, i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+      chr   = this.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  };
+
 const processEveryPhoto = (links) => {
-
+    
     function fn(item, nextFn) {
-        getHTML(item)
-        .then(
-            (html) => {
-                let clsName = ".KL4Bh";
-                let res = $(clsName, html).children().attr('src');
+        let res = getHTML(item)
+            .then(
+                (html) => {
+                    let clsName = ".KL4Bh";
+                    let res = $(clsName, html).children().attr('src');
 
-                return res;
-            },
-            nextFn
-        )
-        .then(nextFn);
+                    return res;
+                }//errors
+            )
+
+        nextFn(null, res);
     }
 
-    asyncf.map(links, fn);
+    let arr = new Array();
 
-    return links;
+    function done(err, res) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        for(let i = 0; i < res.length; i++) {
+            arr[i] = res[i];
+        }
+    }
+
+    asyncf.map(links, fn, done);
+
+    return arr;
 }
 
 let photoLinks = getHTML(url)
@@ -48,7 +76,35 @@ let photoLinks = getHTML(url)
         }
     )
     .then(processEveryPhoto)
-    .then(console.log)
+    .then(
+        (linkArray) => {
+            return Promise.all(linkArray);   
+        }
+    )
+    .then(
+        (linkArray) => {
+            linkArray.map(
+                (item) => {
+                    const options = {
+                        url: item,
+                        encoding: null
+                    }
+                    
+                    function genName(item) {
+                        return PATH + item.hashCode() + ".jpg";
+                    }
+
+                    rp.get(options)
+                        .then(
+                            function (res) {
+                                const buffer = Buffer.from(res, 'utf8');
+                                fs.writeFileSync(genName(item), buffer);
+                            }
+                        );
+                }
+            )
+        }
+    )
     .catch(
         (err) => {
             console.log(err);
